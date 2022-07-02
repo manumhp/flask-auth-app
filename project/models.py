04cab6fab0738b46c66f7b84caafa58f8522a181
Manu Puthiyadath <manumaheshwar.p@gmail.com>
2022-07-02 16:37:02 +0200

    first commit with all the code of the app

---
+++ project/models.py
@@ -0,0 +1,11 @@
+from enum import unique
+from flask_login import UserMixin
+
+from click import password_option
+from .import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
